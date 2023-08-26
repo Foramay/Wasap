eecd7979064cf52728d07b87765358673b49abce
David <davidforastier10@gmail.com>
2023-08-26 18:29:24 -0300

    Maquetado + Estilos Css y JavaScript

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,39 @@
 //Acá voy a dejar todas las funcionalidades de JavaScript
+
+
+//Esta función la cree para que cuando el usuario haga click en el icono de buscar, se conecte automaticamente con el textarea
+document.getElementById('icono').addEventListener('click', function() {
+    document.getElementById('textarea').focus();
+});
+
+
+//Esta función lo que hace es que cuando el usuario haga click sobre el textarea, desaparezca el placeholder
+document.getElementById('textarea').addEventListener('focus', function() {
+    this.removeAttribute('placeholder');
+});
+
+document.getElementById('textarea').addEventListener('blur', function() {
+    if (!this.value) {
+        this.setAttribute('placeholder', 'Busca un chat o inicia uno nuevo.');
+    }
+});
+
+
+//Esta función lo que hace es solamente cambiar de un icono a otro al hacer click en el textarea
+document.getElementById('textarea').addEventListener('focus', function() {
+    this.removeAttribute('placeholder');
+    document.getElementById('icono').classList.replace('fa-magnifying-glass', 'fa-arrow-left');
+});
+
+document.getElementById('textarea').addEventListener('blur', function() {
+    if (!this.value) {
+        this.setAttribute('placeholder', 'Busca un chat o inicia uno nuevo.');
+        document.getElementById('icono').classList.replace('fa-arrow-left', 'fa-magnifying-glass');
+    }
+});
+
+
+
+
+
+
